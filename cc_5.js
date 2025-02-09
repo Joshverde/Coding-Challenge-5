4a679a313972c4ae6efba80240e4ff0c2de1eb80
Joshverde <52259302+Joshverde@users.noreply.github.com>
2025-02-09 13:50:06 -0500

    Task 1 - Customer Profile

---
+++ cc_5.js
@@ -0,0 +1,10 @@
+//Task 1 - Customer Profile
+//Creates an object with 3 properties
+let customer = {
+    name: "John Doe",
+    age: 35,
+    email: "john.doe@email.com"
+};
+//prints to console customer info formated for calrity 
+console.log(`**Task 1**\nCustomer Name: ${customer.name}\nCustomer Age: ${customer.age}\nCustomer Email: ${customer.email}\n`);
+
